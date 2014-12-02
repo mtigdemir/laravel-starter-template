bea62fe78343caec4dbd04d1a7d0a52d0b8400e2
mtigdemir <m_tigdemir@hotmail.com>
2014-12-02 15:13:38 +0200

    Static Language File

---
+++ app/lang/en/static.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    
+    "site_name" => "Website4Fun",
+    //Pages 
+    "faq" => "FAQ",
+    "about" => "About Us",
+    "search" => "Search",
+    "login" => "Login",
+    "logout" => "Logout",
+    "register" => "Register",
+    "welcome" => "Welcome",
+    "home" => "Home",
+);
