ecba0b4858f05d7bb29107eede461f704f0a6e73
mtigdemir <m_tigdemir@hotmail.com>
2014-12-02 15:21:35 +0200

    Layout Title Added with Default Site Name From Language Active HTML li added

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -12,6 +12,7 @@ class BaseController extends Controller {
 		if ( ! is_null($this->layout))
 		{
 			$this->layout = View::make($this->layout);
+                        $this->layout->title = Lang::get('static.site_name');
 		}
 	}
 
